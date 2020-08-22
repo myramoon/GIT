76cea2269ee8ff0cdc056ccfe3a84171413158f8
anamfazal <anamfazal94@gmail.com>
2020-08-22 11:18:26 +0530

    additionally prints dictionary containing each day's wage

--- employee_Wage_Builder.sh
+++ employee_Wage_Builder.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash -x
 
-#ARRAY
-declare -a daily_Wage_Array
+#DICTIONARY
+declare -A daily_Wage
 
 #CONSTANTS
 IS_FULL_TIME=2
@@ -19,16 +19,16 @@ totalWorkingDays=0
  
 function getPerDayWorkHours() {
  
-	case $1 in
+	case $1 in			#attendance_Status as incoming parameter
  
 		$IS_PART_TIME)
-	                workHour=8 ;;
+	                workHour=8 ;;	 #part-time hours 
  
                	$IS_FULL_TIME)
-                       	workHour=16 ;;
+                       	workHour=16 ;;	 #full-time hours 
  
                 *)
-                        workHour=0 ;;
+                        workHour=0 ;;	 #absent hours 
  
         esac
 echo $workHour 
@@ -40,11 +40,13 @@ while [[ $totalWorkHours -lt $MAX_WORKING_HOURS && $totalWorkingDays -lt $MAX_WO
 do
 	totalWorkingDays=$((totalWorkingDays + 1)) 			            #increment the number of days logged
 	workHour="$( getPerDayWorkHours $((RANDOM%3)) )"  			    #Check attendance status using RANDOM function and call the function that calculates work hours	
-	daily_Wage_Array[$totalWorkingDays]=$((workHour * EMPLOYEE_PER_HOUR_RATE))  #store each day's wage in an array
+	daily_Wage["DAY" $totalWorkingDays]=$((workHour * EMPLOYEE_PER_HOUR_RATE))  #store each day's wage in a dictionary
 	totalWorkHours=$((totalWorkHours + workHour))                               #update total working hours logged
 done
+
 echo "Salary of the employee for current month : " $(( totalWorkHours * EMPLOYEE_PER_HOUR_RATE ))  #Print monthly salary
-echo ${daily_Wage_Array[@]}									   #Print array containing each day's wage
+echo ${!daily_Wage[@]} 									   #Print dictionary's keys i.e DAY#	
+echo ${daily_Wage[@]}									   #Print dictionary containing each day's wage
 
 
 
