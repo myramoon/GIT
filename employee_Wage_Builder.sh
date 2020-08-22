71fb072d388a47d47eefa7075c343182a366de25
anamfazal <anamfazal94@gmail.com>
2020-08-22 10:04:08 +0530

    Computes wage for 1 day

--- employee_Wage_Builder.sh
+++ employee_Wage_Builder.sh
@@ -1,16 +1,23 @@
 #!/usr/local/bin/bash
 
 #CONSTANTS
-
 IS_PRESENT=1
+EMPLOYEE_PER_HOUR_RATE=20
+
+#VARIABLES
+workHour=0
+salary=0
 attendance_Status=$((RANDOM%2))  #Check attendance status using RANDOM function
 
-if [[ $attendance_Status -eq $IS_PRESENT ]]
+if [[ $IS_PRESENT -eq $attendance_Status ]]
 then
-	echo "Employee is present."   #Print message according to attendance
+	workHour=8 
 else
-	echo "Employee is absent."
+	workHour=0
 fi
+ 
+echo "Working hours of employee : " $workHour 				#Print work hours for the day and resulting salary.
+echo "salary : " $(( workHour * EMPLOYEE_PER_HOUR_RATE ))
 
 
 
