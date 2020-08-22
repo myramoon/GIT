df02ca26b6258a7b85ca0d8c4f101ac0458e729a
anamfazal <anamfazal94@gmail.com>
2020-08-22 10:09:06 +0530

    Includes part time condition in Daily Wage UC

--- employee_Wage_Builder.sh
+++ employee_Wage_Builder.sh
@@ -1,23 +1,28 @@
 #!/usr/local/bin/bash
 
 #CONSTANTS
-IS_PRESENT=1
+IS_FULL_TIME=2
+IS_PART_TIME=1
 EMPLOYEE_PER_HOUR_RATE=20
-
+ 
 #VARIABLES
 workHour=0
 salary=0
-attendance_Status=$((RANDOM%2))  #Check attendance status using RANDOM function
-
-if [[ $IS_PRESENT -eq $attendance_Status ]]
+attendance_Status=$((RANDOM%3))  #Check attendance status using RANDOM function
+ 
+if [[ $IS_FULL_TIME -eq $attendance_Status ]]
 then
-	workHour=8 
+	workHour=16 
+elif [[ $IS_PART_TIME -eq $attendance_Status ]]
+then
+	workHour=8
 else
 	workHour=0
 fi
  
 echo "Working hours of employee : " $workHour 				#Print work hours for the day and resulting salary.
 echo "salary : " $(( workHour * EMPLOYEE_PER_HOUR_RATE ))
+ 
 
 
 
